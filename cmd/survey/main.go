6dc7e5df849ac48e9ccb413920598c8ed3ec7062
Jean-Baptiste Agostin <jean-baptiste.agostin@abtasty.com>
2020-12-16 11:03:37 +0100

    add folders

---
+++ cmd/survey/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	v1 "github.com/jibe0123/survey/cmd/v1"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	gin.ForceConsoleColor()
+	//err := database.Connect()
+	router := gin.Default()
+
+	v1.ApplyRoutes(router)
+
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Not found",
+		})
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exited")
+}
